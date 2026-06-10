a863a5f8c18d4999fe2906f1b255a3003330add0
Debojeet Mitra <debojeetmitra04@gmail.com>
2026-06-10 22:13:34 +0530

    Todoie-Project done by React

--- 01_app/src/index.js
+++ 01_app/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function MyButton(){
-    return <button>Hello</button>
+
+
+
+function Card(){
+    return <h1>Card</h1>
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<MyButton/>)
+root.render(<Card/>)
---
+++ todoie/src/App.js
@@ -0,0 +1,22 @@
+
+import React from "react"
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
+import Button from "./components/Button";
+
+import './style.css'
+
+const App = () => {
+  return (
+    <div className="todo-container">
+      <Header/>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+      <Button/>
+    </div>
+  )
+};
+
+export default App;
